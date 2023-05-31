c1c0ffeca3cd126c4b6937be9ad631250511ddd4
Alex Mumo <alexymumo2000@gmail.com>
2023-05-31 15:23:36 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version "8.0.0" apply false
+    id("com.android.library") version "8.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.20" apply false
+    id("org.jlleitschuh.gradle.ktlint") version ("11.3.1")
+    id("com.diffplug.spotless") version ("5.17.1")
+}
+
+subprojects {
+    apply(plugin = "com.diffplug.spotless")
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            ktlint()
+            trimTrailingWhitespace()
+            indentWithSpaces()
+            endWithNewline()
+        }
+
+        java {
+            target("**/*.java")
+            googleJavaFormat().aosp()
+            removeUnusedImports()
+            trimTrailingWhitespace()
+            indentWithSpaces()
+            endWithNewline()
+        }
+        format("xml") {
+            target()
+            indentWithSpaces()
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+
+        format("misc") {
+            target("**/*.md", "**/.gitignore")
+            indentWithSpaces()
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
+}
