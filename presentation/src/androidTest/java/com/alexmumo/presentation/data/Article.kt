4a0a352044790e0cf5f3d293e2029c0629bb9d65
Alex Mumo <alexmumoit12@gmail.com>
2024-03-17 10:40:53 +0300

    Add detail screen test

---
+++ presentation/src/androidTest/java/com/alexmumo/presentation/data/Article.kt
@@ -0,0 +1,18 @@
+package com.alexmumo.presentation.data
+
+import com.alexmumo.domain.model.Article
+import com.alexmumo.domain.model.Source
+
+val article = Article(
+    author = null,
+    content = null,
+    description = null,
+    publishedAt = null,
+    source = Source(
+        id = null,
+        name = ""
+    ),
+    title = null,
+    url = "",
+    urlToImage = null
+)
--- presentation/src/androidTest/java/com/alexmumo/presentation/detail/DetailScreenTest.kt
+++ presentation/src/androidTest/java/com/alexmumo/presentation/detail/DetailScreenTest.kt
@@ -1,20 +1,31 @@
 package com.alexmumo.presentation.detail
 
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.navigation.testing.TestNavHostController
+import com.alexmumo.presentation.data.article
 import org.junit.Rule
+import org.junit.Test
 
 
 class DetailScreenTest {
 
-    @get:Rule
+    @get:Rule(order = 1)
     val composeRule = createComposeRule()
 
+    lateinit var navController: TestNavHostController
 
+    @Test
     fun testDetailScreen() {
         composeRule.setContent {
-            DetailScreen(navController = , article = )
+            DetailScreen(navController = navController, article = article)
         }
+        composeRule.onNodeWithTag("detail_tag").assertExists()
+        composeRule.onNodeWithTag()
+        composeRule.onNodeWithText("Already Bookmarked").assertExists()
+        composeRule.onNodeWithTag("detail_screen").assertExists()
 
     }
-
 }
