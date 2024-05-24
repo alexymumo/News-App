e3d8c1dbc2438268dfc0112b254ce1ac84744ba9
Alex Mumo <alexymumo2000@gmail.com>
2024-05-24 14:26:16 +0300

    Fix spotless

--- presentation/src/test/java/com/alexmumo/presentation/components/DeleteCardTest.kt
+++ presentation/src/test/java/com/alexmumo/presentation/components/DeleteCardTest.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2024 News-App
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alexmumo.presentation.components
 
 import androidx.compose.material3.rememberDismissState
@@ -10,7 +25,6 @@ import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.shadows.ShadowLog
 
-
 @RunWith(RobolectricTestRunner::class)
 class DeleteCardTest {
 
@@ -19,7 +33,7 @@ class DeleteCardTest {
 
     @Before
     @Throws(Exception::class)
-    fun setUp(){
+    fun setUp() {
         ShadowLog.stream = System.out
     }
 
